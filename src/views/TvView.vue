e6b92bd7c5cc8d8f44589b855b4ec52910ecf82b
rafaelabarbierc <rafaelabarbieric@gmail.com>
2024-12-02 16:25:35 -0300

    feat: listagem de progamas de tv

--- src/views/TvView.vue
+++ src/views/TvView.vue
@@ -8,17 +8,50 @@
     const response = await api.get('genre/tv/list?language=pt-BR');
     genres.value = response.data.genres;
   });
+
+  const tv = ref([]);
+
+  const listTv = async (genreId) => {
+      const response = await api.get('discover/tv', {
+          params: {
+              with_genres: genreId,
+              language: 'pt-BR'
+          }
+      });
+      tv.value = response.data.results
+  };
 </script>
 
 <template>
   <div>
     <h1>Gêneros de programas de TV</h1>
-    <ul>
-      <li v-for="genre in genres" :key="genre.id">
-        {{ genre.name }}
+    <ul class="genre-list">
+      <li
+      v-for="genre in genres"
+      :key="genre.id"
+      @click="listTv(genre.id)"
+      class="genre-item"
+      >
+      {{ genre.name }}
       </li>
     </ul>
   </div>
+
+  <div class="tv-list">
+  <div v-for="tvs in tv" :key="tvs.id" class="tv-card">
+
+    <img
+      :src="`https://image.tmdb.org/t/p/w200${tvs.poster_path}`"
+      :alt="tvs.title"
+    />
+    <div class="tv-details">
+      <p class="tv-title">{{ tvs.name }}</p>
+      <p class="tv-release-date">{{ tvs.first_air_date }}</p>
+      <p class="tv-genres">{{ tvs.genre_ids }}</p>
+    </div>
+
+  </div>
+</div>
 </template>
 
 <style scoped>
@@ -47,4 +80,4 @@
     box-shadow: 0 0 0.5rem #5d6424;
   }
 </style>
-  
+
